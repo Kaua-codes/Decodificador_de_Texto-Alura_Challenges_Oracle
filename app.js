e487addff307aa6ffb056e4efec403399b15cdd6
Kaua <kauchomom@gmail.com>
2024-08-23 00:05:01 -0300

    WIP: Working on home page for desktop

---
+++ app.js
@@ -0,0 +1,18 @@
+function criptografarMensagem() {
+  let mensagem = document.querySelector("textarea").value;
+  console.log(mensagem);
+}
+
+function descriptografarMensagem() {
+  let mensagem = document.querySelector("textarea").value;
+  console.log(mensagem);
+}
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+function validarMensagem() {
+  if (mensagem)
+}
