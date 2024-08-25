7fcbac44d684c8df0e1f98ba961e26a10ebe033e
Kaua <kauchomom@gmail.com>
2024-08-24 23:44:04 -0300

    WIP: Working on home page for desktop

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+let mensagem;
+let statusValidadeMensagem;
+
+const expressaoRegularMinusculas = /^[a-z]+$/;
+
 function criptografarMensagem() {
-  let mensagem = document.querySelector("textarea").value;
+  mensagem = document.querySelector("textarea").value;
   console.log(mensagem);
+
+  validarMensagem(mensagem);
 }
 
 function descriptografarMensagem() {
@@ -13,6 +20,38 @@ function exibirTextoNaTela(tag, texto) {
   campo.innerHTML = texto;
 }
 
-function validarMensagem() {
-  if (mensagem)
+function validarMensagem(mensagem) {
+  statusValidadeMensagem = expressaoRegularMinusculas.test(mensagem);
+
+  console.log("Status validade mensagem: " + statusValidadeMensagem);
+
+  if (statusValidadeMensagem) {
+    // Seleciona o parágrafo de classe home-page__conteiner-input__others__alert
+    let alertaParagrafo = document.querySelector(
+      ".home-page__conteiner-input__others__alert"
+    );
+
+    // Seleciona a imagem dentro do parágrafo
+    let alertaImagem = alertaParagrafo.querySelector("img");
+
+    // Altera o atributo 'src' da imagem
+    alertaImagem.src = "/assets/check-circle-fill.svg";
+
+    // Altera o texto do parágrafo, mantendo a imagem
+    alertaParagrafo.innerHTML = `<img src="${alertaImagem.src}" alt="Logo de check"/> Texto valido.`;
+  } else {
+    // Seleciona o parágrafo de classe home-page__conteiner-input__others__alert
+    let alertaParagrafo = document.querySelector(
+      ".home-page__conteiner-input__others__alert"
+    );
+
+    // Seleciona a imagem dentro do parágrafo
+    let alertaImagem = alertaParagrafo.querySelector("img");
+
+    // Altera o atributo 'src' da imagem
+    alertaImagem.src = "/assets/exclamation-circle-fill.svg";
+
+    // Altera o texto do parágrafo, mantendo a imagem
+    alertaParagrafo.innerHTML = `<img src="${alertaImagem.src}" alt="Logo de alerta"/> Apenas letras minúsculas e sem acento.`;
+  }
 }
