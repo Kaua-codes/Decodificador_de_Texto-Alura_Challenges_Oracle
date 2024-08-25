800f72d42371e5f996cde5b7fe8f26e33b5d68dd
Kaua <kauchomom@gmail.com>
2024-08-25 14:53:05 -0300

    WIP: Working on home page for desktop

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
 let mensagem;
 let statusValidadeMensagem;
+let chavesCriptografia = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
+};
+let chavesDescriptografia = {
+  enter: "e",
+  imes: "i",
+  ai: "a",
+  ober: "o",
+  ufat: "u",
+};
 
 const expressaoRegularMinusculas = /^[a-z]+$/;
 
 function criptografarMensagem() {
   mensagem = document.querySelector("textarea").value;
-  console.log(mensagem);
+  console.log("Mensagem original: " + mensagem);
 
   validarMensagem(mensagem);
+  console.log("Status validade mensagem: " + statusValidadeMensagem);
+
+  if (statusValidadeMensagem) {
+    let mensagemCriptografada = substituirLetra(mensagem, chavesCriptografia);
+    console.log("Mensagem criptografada: " + mensagemCriptografada);
+  } else {
+    limparTextarea();
+  }
 }
 
 function descriptografarMensagem() {
-  let mensagem = document.querySelector("textarea").value;
-  console.log(mensagem);
+  mensagem = document.querySelector("textarea").value;
+  console.log("Mensagem original: " + mensagem);
+
+  validarMensagem(mensagem);
+  console.log("Status validade mensagem: " + statusValidadeMensagem);
+
+  if (statusValidadeMensagem) {
+    let mensagemDescriptografada = substituirTexto(
+      mensagem,
+      chavesDescriptografia
+    );
+    console.log("Mensagem descriptografada: " + mensagemDescriptografada);
+  } else {
+    limparTextarea();
+  }
 }
 
 function exibirTextoNaTela(tag, texto) {
@@ -20,10 +55,35 @@ function exibirTextoNaTela(tag, texto) {
   campo.innerHTML = texto;
 }
 
+function substituirLetra(textoOriginal, substituicoes) {
+  let textoModificado = "";
+
+  for (let i = 0; i < textoOriginal.length; i++) {
+    let letraAtual = textoOriginal[i];
+
+    if (substituicoes[letraAtual]) {
+      textoModificado += substituicoes[letraAtual];
+    } else {
+      textoModificado += letraAtual;
+    }
+  }
+  return textoModificado;
+}
+
+function substituirTexto(textoOriginal, substituicoes) {
+  let textoModificado = textoOriginal;
+
+  // Percorre todas as substituições e as aplica no texto original
+  for (let [chave, valor] of Object.entries(substituicoes)) {
+    let regex = new RegExp(chave, "g");
+    textoModificado = textoModificado.replace(regex, valor);
+  }
+
+  return textoModificado;
+}
+
 function validarMensagem(mensagem) {
   statusValidadeMensagem = expressaoRegularMinusculas.test(mensagem);
-
-  console.log("Status validade mensagem: " + statusValidadeMensagem);
 
   if (statusValidadeMensagem) {
     // Seleciona o parágrafo de classe home-page__conteiner-input__others__alert
@@ -39,7 +99,7 @@ function validarMensagem(mensagem) {
 
     // Altera o texto do parágrafo, mantendo a imagem
     alertaParagrafo.innerHTML = `<img src="${alertaImagem.src}" alt="Logo de check"/> Texto valido.`;
-  } else {
+  } else if (mensagem != "") {
     // Seleciona o parágrafo de classe home-page__conteiner-input__others__alert
     let alertaParagrafo = document.querySelector(
       ".home-page__conteiner-input__others__alert"
@@ -53,5 +113,24 @@ function validarMensagem(mensagem) {
 
     // Altera o texto do parágrafo, mantendo a imagem
     alertaParagrafo.innerHTML = `<img src="${alertaImagem.src}" alt="Logo de alerta"/> Apenas letras minúsculas e sem acento.`;
+  } else {
+    // Seleciona o parágrafo de classe home-page__conteiner-input__others__alert
+    let alertaParagrafo = document.querySelector(
+      ".home-page__conteiner-input__others__alert"
+    );
+
+    // Seleciona a imagem dentro do parágrafo
+    let alertaImagem = alertaParagrafo.querySelector("img");
+
+    // Altera o atributo 'src' da imagem
+    alertaImagem.src = "/assets/bi_exclamation-circle-fill.svg";
+
+    // Altera o texto do parágrafo, mantendo a imagem
+    alertaParagrafo.innerHTML = `<img src="${alertaImagem.src}" alt="Logo de alerta"/> Apenas letras minúsculas e sem acento.`;
   }
+}
+
+function limparTextarea() {
+  mensagem = document.querySelector("textarea");
+  mensagem.value = "";
 }
